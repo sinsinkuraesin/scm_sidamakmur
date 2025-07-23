656f6877306f66799bebb5fcd24777518bac4cdf
dianaklf4 <dianakholifahnur@gmail.com>
2025-07-23 14:13:16 +0700

    Edit Invoice Penjualan

--- resources/views/admin/jual/invoice.blade.php
+++ resources/views/admin/jual/invoice.blade.php
@@ -129,12 +129,12 @@
         $waNumber = Str::startsWith($noTlp, '0') ? '62' . substr($noTlp, 1) : $noTlp;
 
         $pesan = urlencode("Halo {$jual->konsumen->nama_konsumen}, berikut kami berikan invoice pembelanjaan Anda di *PD Sidamakmur*:\n\n" .
-            "🧾 No Invoice: INV-JUAL-{$jual->jual_id}\n📅 Tanggal: " . \Carbon\Carbon::parse($jual->tgl_jual)->format('d M Y') . "\n" .
-            "💰 Total Pembelian: Rp " . number_format($jual->detailJual->sum('total'), 0, ',', '.') . "\n\n" .
+            "No Invoice: INV-JUAL-{$jual->jual_id}\n📅 Tanggal: " . \Carbon\Carbon::parse($jual->tgl_jual)->format('d M Y') . "\n" .
+            "Total Pembelian: Rp " . number_format($jual->detailJual->sum('total'), 0, ',', '.') . "\n\n" .
             "Silakan lakukan pembayaran melalui transfer ke salah satu rekening berikut:\n\n" .
-            "🏦 *Bank BRI*\nNo. Rek: 1234 5678 9101 1121\nA.n: Nama Pemilik\n\n" .
-            "🏦 *Bank Mandiri*\nNo. Rek: 9876 5432 1098 7654\nA.n: Nama Pemilik\n\n" .
-            "💬 Jika melakukan pembayaran secara *tunai (cash)*, mohon tetap lakukan konfirmasi melalui WhatsApp ini ya 🙏\n\n" .
+            "*Bank BRI*\nNo. Rek: 1234 5678 9101 1121\nA.n: Nama Pemilik\n\n" .
+            "*Bank Mandiri*\nNo. Rek: 9876 5432 1098 7654\nA.n: Nama Pemilik\n\n" .
+            "Jika melakukan pembayaran secara *tunai (cash)*, mohon tetap lakukan konfirmasi melalui WhatsApp ini ya 🙏\n\n" .
             "Terima kasih telah berbelanja di *PD Sidamakmur*.");
 
         $waLink = "https://wa.me/$waNumber?text=$pesan";
